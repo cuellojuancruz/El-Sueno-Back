aab9eb3361e4fae6e86d546afff8687e517c28dd
cuellojuancruz <cuellojuancruz11@gmail.com>
2022-06-04 01:16:15 -0300

    implementacion del Search no terminada (debuggear)

--- src/controllers/product/getProduct.js
+++ src/controllers/product/getProduct.js
@@ -1,31 +1,59 @@
+const { Op } = require('sequelize');
 const { Product } = require("../../db");
+const product = require("../../router/product/product");
 
 
 const getProducts = async (req, res, next) => {
-	try {
-		
-        
-		let product = await Product.findAll();
-		return res.json(product);
-	} catch (err) {
-		console.log(product + "entro al error")
-		next(err);
+	console.log("Entro al back del get")
+	let {name} = req.query
+
+
+	if(!name){
+		try {
+			
+			
+			let product = await Product.findAll();
+			return res.json(product);
+		} catch (err) {
+			console.log(err + "entro al error")
+			next(err);
+		}
+	}
+	else{
+		console.log("entro al else del get")
+		try{
+			product = await Product.findAll({
+				where:{
+					name: {
+						[Op.iLike]: `%${name}%`
+					}
+				}
+			})
+			console.log(product)
+			return res.json(product)
+		}catch (err) {
+			console.log(product + "entro al error")
+			next(err);
+		}
 	}
 };
 
 const getNewProducts = async (req, res, next) => {
+	console.log("entro al back del newproducts")
 	let id = req.params.id
 	
 	try{
-		let newProducts = await Product.findAll({ id: id })
+		let newProducts = await Product.findAll({ limit: id })
 		return res.json(newProducts)
 	} catch (err) {
-		console.log(product + "entro al error")
+		console.log(err + "entro al error")
 		next(err);
 	}
 }
 
 
+
 module.exports = {
     getProducts,
+	getNewProducts,
 }
--- src/router/product/product.js
+++ src/router/product/product.js
@@ -4,7 +4,7 @@ const { createProduct } = require("../../controllers/product/createProduct");
 const { deleteProduct } = require('../../controllers/product/deleteProduct');
 const { getProducts } = require('../../controllers/product/getProduct');
 const { updateProduct } = require('../../controllers/product/updateProduct');
-
+const { getNewProducts } = require('../../controllers/product/getProduct')
 
 const product = Router();
 
@@ -13,7 +13,7 @@ product.delete("/deleteProduct/:id", deleteProduct)
 // product.put("/updateProdut/:id", () => console.log("entro al put"))
 product.put("/updateProdut/:id", updateProduct)
 product.get("/getProduct", getProducts)
-product.get("/getProduct/:id", getProducts)
+product.get("/getProduct/:id", getNewProducts)
 
 
 product.get("/error", (req, res) => {
